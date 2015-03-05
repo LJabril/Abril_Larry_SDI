4048d67f93ecbc1cd535df71ba68659e89655a7b
LJabril <labrilrivera@fullsai.edu>
2015-03-05 14:54:25 -0500

    day2Intro
    Created HTML and js in day 2

---
+++ Day2/js/script.js
@@ -0,0 +1,20 @@
+/*
+Larry Abril
+3/5/2015
+Section 00
+Day 2 Intro
+ */
+
+//alert("Testing to see if connected");
+
+/*
+Example of a multi-line comment.
+Anything inside of here will not be seen by the computer.
+ */
+
+// Single line comment. ONLY this line will be ignored.
+
+// Alert box-pop up box that will alert the user with information.
+
+alert("Text that the user will see.");
+
