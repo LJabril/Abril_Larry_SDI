248ad6ed06595ee0bfc1c97cad010ab5505f49ae
LJabril <labrilrivera@fullsai.edu>
2015-03-05 20:24:23 -0500

    Average Shopping bill and Discounts
    Created a grocery bills for 5 weeks and get average per week. Also
    created an item with original price and found the discount, tax and
    without tax

--- Day 2 LabExpression/js/script.js
+++ Day 2 LabExpression/js/script.js
@@ -37,4 +37,60 @@ console.log(reminderForSparky);
 
 console.log("Sparky got " + reminderForSparky + " slices of pizza.");
 
+// Average shopping bill
+
+//Find a weekly grocery bills.
+var week1 = 79;
+var week2 = 84;
+var week3 = 56;
+var week4 = 62;
+var week5 = 71;
+var totalSpendOnGrocery = week1 + week2 + week3 + week4 + week5;
+console.log(totalSpendOnGrocery);
+
+//Average weekly grocery spending.
+var averageWeeklyGrocerySpending = totalSpendOnGrocery / 5;
+console.log(averageWeeklyGrocerySpending);
+
+//Result Format.
+console.log("You have spent a total of $" + totalSpendOnGrocery + " on groceries over 5 weeks.\nThat is an average of " + averageWeeklyGrocerySpending + "% per week.");
+
+
+//Discount
+//Givens.
+
+var originalPrice = 399.99;
+console.log(originalPrice);
+
+var discountPercentage = 40;
+console.log(discountPercentage);
+
+var discountDecimal = discountPercentage/100;
+console.log(discountDecimal);
+
+var descriptionOfItem = "New iWatch";
+console.log(descriptionOfItem);
+
+var salesTaxPercentage = 7
+var salesTaxDecimal = salesTaxPercentage/100;
+console.log(salesTaxDecimal);
+
+
+var discountAmount = originalPrice * discountDecimal;
+console.log(discountAmount);
+
+
+
+//Result Variables
+
+var priceOfTheItemWithoutTax = originalPrice - discountAmount;
+console.log(priceOfTheItemWithoutTax);
+
+var saleTaxAmount = priceOfTheItemWithoutTax * salesTaxDecimal;
+console.log(saleTaxAmount);
+
+var priceOfTheItemWithTax = priceOfTheItemWithoutTax + saleTaxAmount;
+console.log(priceOfTheItemWithTax);
+
+console.log("Your " + descriptionOfItem + " was originally $" + originalPrice + " , but after a " + discountPercentage + "% discount, it is now " + priceOfTheItemWithoutTax + "\nwithout tax, and $" + priceOfTheItemWithTax + " with tax.");
 
