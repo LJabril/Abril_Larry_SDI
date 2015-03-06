db1f9b828a4f16093950c8bbb2f11b064ad2fe0c
LJabril <labrilrivera@fullsai.edu>
2015-03-05 19:19:49 -0500

    Slice of Pie part I & II
    Created slices per pizza, people at the party, pizza ordered, slices
    per person, how many pizza people eat, and sparky’s leftover

--- Day 2 LabExpression/js/script.js
+++ Day 2 LabExpression/js/script.js
@@ -7,3 +7,34 @@ Day 2 Lab Expression
 
 alert("Testing to see if connected");
 
+// Slice of Pie part 1
+
+var slicesPerPizza = 8 ;
+var peopleAtTheParty = 36 ;
+var pizzasOrdered = 6;
+
+//Number of slide per person.
+// Multiply the pizzas ordered by the slides per pizza and then divide in the people at the party.
+
+var slicesPerPerson = (pizzasOrdered * slicesPerPizza) / peopleAtTheParty;
+console.log(slicesPerPerson);
+
+// Result Format.
+console.log("Each person ate " + slicesPerPerson + " slices of pizza at the party.");
+
+//Slice of Pie part II
+var dog = "sparky";
+console.log("The host's dog name is " + dog);
+
+// Sparky is hungry and ready to eat the leftover.
+// We are going to use the modulo to get the sparky' pizza
+
+var reminderForSparky = (pizzasOrdered * slicesPerPizza) % peopleAtTheParty;
+console.log(reminderForSparky);
+
+//Sparky is happy! Too many pizza.
+//Result Format.
+
+console.log("Sparky got " + reminderForSparky + " slices of pizza.");
+
+
