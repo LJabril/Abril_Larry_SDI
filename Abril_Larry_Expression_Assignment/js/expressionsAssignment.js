c375306e0935011b7005e6e10b14e2bb6d2c4096
LJabril <labrilrivera@fullsai.edu>
2015-03-07 18:46:13 -0500

    Use prompt
    Use prompt to find the monthly bill amount.

--- Abril_Larry_Expression_Assignment/js/expressionsAssignment.js
+++ Abril_Larry_Expression_Assignment/js/expressionsAssignment.js
@@ -15,4 +15,18 @@ Expressions Assignment
  //check to see if the array is ok.
  console.log(bill);
 
+//using prompt to put value to the bills.
+ var electricity =prompt("Please enter the amount of your monthly electrical bill.");
+ var water = prompt("your monthly water bill.");
+ var cable = prompt("your monthly cable bill.");
+ var carInsurance = prompt("your monthly car insurance bill.");
+ var rent = prompt("your monthly rent bill.");
+
+//find the total of the prompts.
+var totalMonthlyBills = parseInt(electricity) + parseInt(water) + parseInt(cable) + parseInt(carInsurance) + parseInt(rent);
+
+//Result of your prompt.
+ console.log("If you pay all your bills in full and on time. Your monthly bill amount is $" + totalMonthlyBills + ".");
+
+
 
