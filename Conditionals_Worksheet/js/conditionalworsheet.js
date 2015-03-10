d304bd5bf18ed0edbbacfe3bd09177be2c368baa
LJabril <labrilrivera@fullsai.edu>
2015-03-10 19:11:40 -0400

    Group 1 excersice
    worked in last chance for gas

---
+++ Conditionals_Worksheet/js/conditionalworsheet.js
@@ -0,0 +1,31 @@
+
+ /*
+Larry Abril
+3/10/2015
+Section 00
+Conditionals Worksheet
+ */
+
+
+//alert("Just testing!");
+
+// Group 1 Last Chance for Gas
+//Create variables
+ var efficiencyOfTheCar = prompt ("Enter the gas mpg for your car");
+ var gaugeReadingOnTheGasTank = prompt("Enter how much gas do you have in your tank right now in %");
+ var gasTankCapacity = prompt("Enter the gas tank capacity for you car");
+
+ //verify how much gallons and miles are left
+ var gallonsLeft= ( gaugeReadingOnTheGasTank / 100 ) * gasTankCapacity;
+ console.log("You have " + gallonsLeft + " gallons left.");
+ var milesLeft =gallonsLeft * efficiencyOfTheCar;
+ console.log("You have " + milesLeft + " miles left.");
+
+//using || to find out if you need to stop or keep driving
+ if (milesLeft <= 200){
+     console.log("You need to stop to get gas.");
+ } else {
+     console.log("You can keep driving.");
+ }
+
+
