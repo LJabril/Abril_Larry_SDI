281b3d11e5dacf2e81eb6bec92e2c1f306fed817
LJabril <labrilrivera@fullsai.edu>
2015-03-11 11:58:18 -0400

    Group 3
    I have trouble in this problem. It was hard.

--- Conditionals_Worksheet/js/conditionalworsheet.js
+++ Conditionals_Worksheet/js/conditionalworsheet.js
@@ -29,6 +29,7 @@ Conditionals Worksheet
  }
 */
 
+ /*
  // Group 2 Check the login
  //create variables
  var newUserName = prompt("enter a new user name");
@@ -53,3 +54,33 @@ var savedPassword = newPassword;
 } else if ((newUserName == verifyUser) && (newPassword != verifyPass)){
      console.log("Password does not match our records.");
  }
+*/
+
+ //Group 3 Movie Ticket Price
+ //Create variables
+ var ticketPrice = 12;
+ var age = prompt("Enter your age");
+ var discount = 7;
+ var timeDiscount = prompt("Enter time");
+ var ticketAfterDiscount = ticketPrice - discount;
+
+ /*if ((age <=55) && (age >=10)){
+     console.log("You have a $" + discount+". You ticket will be $" + ticketAfterDiscount + ".");
+ } else {
+     console.log("You pay $" + ticketPrice + " regular price.");
+ }
+ */
+
+var result;
+ result = (age <= 55 && age >= 10) ?   "You pay "+ticketPrice+"." : "You get $ "+discount+".";
+ console.log(result);
+
+var generalDiscount;
+ generalDiscount= (timeDiscount >= 3 && timeDiscount <= 5) ? "You pay "+ticketPrice+"." : "You will paid $" + ticketAfterDiscount+ ".";
+ console.log(generalDiscount);
+
+ //This section gave me trouble.
+ //I stayed i
+ // the classroom the entire time for the lab but the time end up and I went home.
+ //I spend some time on it but I couldn't figure it out.
+ 
