751ec8283e2309bf0317128f360153c241a34c46
LJabril <labrilrivera@fullsai.edu>
2015-03-10 20:08:26 -0400

    Group 2
    worked on the user name and password project

--- Conditionals_Worksheet/js/conditionalworsheet.js
+++ Conditionals_Worksheet/js/conditionalworsheet.js
@@ -11,7 +11,7 @@ Conditionals Worksheet
 
 // Group 1 Last Chance for Gas
 //Create variables
- var efficiencyOfTheCar = prompt ("Enter the gas mpg for your car");
+ /*var efficiencyOfTheCar = prompt ("Enter the gas mpg for your car");
  var gaugeReadingOnTheGasTank = prompt("Enter how much gas do you have in your tank right now in %");
  var gasTankCapacity = prompt("Enter the gas tank capacity for you car");
 
@@ -27,5 +27,29 @@ Conditionals Worksheet
  } else {
      console.log("You can keep driving.");
  }
+*/
 
+ // Group 2 Check the login
+ //create variables
+ var newUserName = prompt("enter a new user name");
+ var newPassword = prompt("enter a new password");
+ var verifyUser = prompt(" Please verify your user name");
+ var verifyPass = prompt("Please verify your password");
+ var savedName = newUserName;
+var savedPassword = newPassword;
 
+ // verify user name and password
+ // result if user and password are correct
+ //Result if user does not match
+ //Result if password does not match
+
+ if ((newUserName == verifyUser) && (newPassword == verifyPass)){
+     console.log("Welcome, your login was successful");
+ }
+
+ if ((newUserName != verifyUser) && (newPassword == verifyPass)){
+    console.log("User not found. Try again!");
+
+} else if ((newUserName == verifyUser) && (newPassword != verifyPass)){
+     console.log("Password does not match our records.");
+ }
