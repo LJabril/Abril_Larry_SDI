ba0d5c753482c5020cddbd7e4fa51e8755603250
LJabril <labrilrivera@fullsai.edu>
2015-03-14 16:58:58 -0400

    created for loop
    created for loop

--- Day 6/Day 6 for loops/js/day6ForLoops.js
+++ Day 6/Day 6 for loops/js/day6ForLoops.js
@@ -7,4 +7,43 @@ Day 6 for Loops
  */
 
 
-alert("Just testing!");
+//alert("Just testing!");
+
+ //for(initialization; condition; increment of change) {}
+ //we use i A LOT in loops because it stands for interaction
+
+ for (var i =0; i<20; i+=1){
+     console.log("The value of the i is " + i);
+ }
+ console.log(i);
+
+ //Break in a loop
+
+ for (var j =0; j<5; j++){
+     //conditional to test if we should break the loop
+     //if the value of j is 3 console.log something different and stop the loop
+
+     if (j === 3 ){
+         //stop the loop but first console.log something
+         console.log("J is 3 !!!");
+         break;
+     }
+     console.log("The value of is " + j);
+
+ }
+
+ //Specific cases for each value
+
+ for (var k = 0; k<3; k++){
+
+     if (k === 0){
+         console.log("The value is zero");
+     } else if (k===1){
+         console.log("The value is one");
+     } else if (k === 2){
+         console.log("The value is two");
+     }
+
+
+ }
+
