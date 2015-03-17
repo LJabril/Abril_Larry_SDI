a5cf8c2a156dbb61f80b33a9fef5fb834beef1d6
LJabril <labrilrivera@fullsai.edu>
2015-03-17 14:09:21 -0400

    for loops for zombie numbers
    for loops for zombie numbers

--- Day 7/Zombies/js/day7zombies.js
+++ Day 7/Zombies/js/day7zombies.js
@@ -7,4 +7,39 @@ Day 7 Zombies
  */
 
 
-alert("Just testing!");
+//alert("Just testing!");
+
+ //Zombies Attack
+ // We have accidentally a zombie at Full Sail.
+ //A Zombie can bite 4 people a day  and turn them into zombie the next day.
+ //The CDC wants to know how many Zombie there will be in 8 days?
+
+ //Givens that we know.
+
+ //How many Zombie do we have
+
+ var numZombies= 1;
+
+ //How many bites per Zombies per day
+ var numBites= 4;
+
+ //Numbers of day that CDC wants
+ var days=8;
+
+ // create for Loops to calculate zombie number
+ for (var i=1; i<=days; i++){
+
+     //how many new zombie get made every day
+     var newZombie= numZombies*numBites;
+
+     //add the new zombie to our exhausted horde
+     numZombies+= newZombie;
+
+     //console.log(""); the results
+     console.log("There are " + numZombies + " number of zombie on day #" + i+ "!");
+ }
+
+
+
+ //how long will it takes to get a million zombies
+ 
