c7955bc00e7c9f49312d6d707ba4b05bf3f2f207
LJabril <labrilrivera@fullsai.edu>
2015-03-17 18:56:21 -0400

    create a return for problem 1
    create a return for problem 1

--- Functions_worksheet/js/functionWorksheet.js
+++ Functions_worksheet/js/functionWorksheet.js
@@ -5,6 +5,20 @@ Larry Abril
 Section 00
 Functional Worksheet
  */
+var radius = 1;
+
+//alert("Just testing!");
 
 
-alert("Just testing!");
+ // create a function to calculate a circle circumference
+ function circleParameter(r){
+//calc area = 2 * PI * r
+     var circumference= 2 * Math.PI * r;
+//return the value
+     return circumference;
+ }
+
+ var circParameter = circleParameter(radius);
+ //console.log(); the results
+ console.log("The area of the circle is "+ circParameter);
+
