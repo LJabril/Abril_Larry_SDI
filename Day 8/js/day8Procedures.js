49bb6238351167cb0e39df859e83856bc9170d86
LJabril <labrilrivera@fullsai.edu>
2015-03-24 15:09:52 -0400

    Learned Procedures
    Learned the difference between procedure and basic function

--- Day 8/js/day8Procedures.js
+++ Day 8/js/day8Procedures.js
@@ -7,4 +7,32 @@ Day 8 Procedures
  */
 
 
-alert("Just testing!");
+//alert("Just testing!");
+
+ //This is a BASIC FUNCTION
+ function calcAreaF (w,h){
+     var area = w*h;
+     return area;
+ }
+ //function call
+ var area = calcAreaF(6,8);
+ console.log("Te area of the rectangle is " + area+ ".");
+
+
+
+
+
+ //This is a PROCEDURE
+ //A list of step to do.
+
+ function calcAreaP (w,h){
+     var area =w*h;
+
+     //we do not RETURN this value
+     console.log("Inside the procedure is " +area +".");
+
+ }
+
+ // FUNCTION CALL for the PROCEDURE?
+ //just the Function Name ... NO variable to catch it
+ calcAreaP(12,16);
